6c25048bad0ed7bdf84c2e556efb23ca589fc1a5
PyoMyeongchan <vyaudcks@gmail.com>
2025-06-09 14:50:57 +0900

    monsterController / CatchSceneController

--- Assets/02.Scripts/Controller/MonsterController.cs
+++ Assets/02.Scripts/Controller/MonsterController.cs
@@ -1,9 +1,38 @@
+using FoodyGo.Database;
+using FoodyGo.Mapping;
+using FoodyGo.Services;
 using UnityEngine;
 
 namespace FoodyGo.Controller
 {
-    public class MonsterController
+    public class MonsterController : MonoBehaviour
     {
-
+        public MapLocation location;
+        public MonsterService monsterService;
+        public Monster monsterDataObject;
+        public Animator animator;
+        public float animatorSpeed;
+        public float monsterWarmRate = .0001f;
+        
+        // Use this for initialization
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+            animator.speed = animatorSpeed;
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            if (animatorSpeed == 0)
+            {
+                //monster is frozen solid
+                animatorSpeed = 0;
+                return;
+            }
+            //if monster is moving they will warm up a bit
+            animatorSpeed = Mathf.Clamp01(animatorSpeed + monsterWarmRate);
+            animator.speed = animatorSpeed;
+        }
     }
 }
---
+++ Assets/02.Scripts/InputSystems/CustomTouchPad.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace FoodyGo.InputSystems
+{
+    public class CustomTouchPad : MonoBehaviour
+    {
+        
+    }
+    
+    
+}
