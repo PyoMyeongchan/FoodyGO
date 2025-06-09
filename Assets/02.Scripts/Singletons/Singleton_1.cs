e85c1dbb0458d26ea4909a9f77c4dca50b372ad1
PyoMyeongchan <vyaudcks@gmail.com>
2025-06-09 13:59:06 +0900

    싱글톤

---
+++ Assets/02.Scripts/Singletons/Singleton_1.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace FoodyGo.Singletons
+{
+    public abstract class Singleton_1<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    T component = FindAnyObjectByType<T>();
+
+                    if (component == null)
+                    {
+                        GameObject empty = new GameObject(typeof(T).Name);
+                        component = empty.AddComponent<T>();
+                    }
+                    
+                    _instance = component;
+                    DontDestroyOnLoad(_instance.gameObject);
+                }
+                return _instance;
+            }
+        }
+        
+        private static T _instance;
+    }
+}
